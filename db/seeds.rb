6440374ce3744293470d616caeb9ba4ef1637809
benedikt conze <benedikt@conze.de>
2022-05-23 17:32:30 +0200

    edited mentor index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+expertise = ["tech", "finance", "consulting"]
+
+10.times do
+  user = User.new(name: Faker::Name.name, email: Faker::Internet.email, password: "123456")
+  user.save
+  mentor = Mentor.new(title: Faker::Company.buzzword, expertise: "#{expertise.sample}", description: Faker::Company.catch_phrase, user: user)
+  mentor.save
+end
